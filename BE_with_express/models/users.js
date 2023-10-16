70b7c8218894b17016c3f3d4155d0d827cc605c5
Pseheyon <100889975+Pseheyon@users.noreply.github.com>
2023-10-16 19:33:29 +0900

    [error] 파일 삭제 에러 확인중

--- BE_with_express/models/users.js
+++ BE_with_express/models/users.js
@@ -18,37 +18,15 @@ const userSchema = new mongoose.Schema({
   userId: {
     type: String,
     required: true,
-<<<<<<< HEAD
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-=======
   },
   nickname: {
     type: String,
     required: true,
->>>>>>> 821e9bdc250bf3f11062b4e0c1d98a5ac59edd10
   },
   password: {
     type: String,
     required: true,
   },
-<<<<<<< HEAD
-  confirmpassword: {
-    type: String,
-    required: true,
-  },
-  comment: String, // 옵션에 type밖에 없을 경우 간단하게 표현 할 수 있다.
-  createdAt: {
-    type: Date,
-    default: Date.now, // 기본값
-  },
-});
-module.exports = mongoose.model("Comments", userSchema);
-=======
   confirm_password: {
     type: String,
     required: true,
@@ -56,4 +34,3 @@ module.exports = mongoose.model("Comments", userSchema);
 });
 
 module.exports = mongoose.model("User", userSchema);
->>>>>>> 821e9bdc250bf3f11062b4e0c1d98a5ac59edd10
